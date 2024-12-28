25713be5b3a27ed0fa5cb1d6d91d3c44eaf3e6d8
samuel <samuelgomes101112@gmail.com>
2024-12-28 12:46:06 -0300

    HEY-2: installing pint

--- anotacoes.js
+++ anotacoes.js
@@ -2,3 +2,7 @@
 
 // Para aux. no git hooks, para que fique salvo no git os commits padroes do projeto, iremos instalar um pacote JS:
 // --husky git hooks https://typicode.github.io/husky/
+
+
+// LARAVEL PINT
+// formatador de codigo, mais bonito
