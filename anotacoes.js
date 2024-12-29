e334c710dfd9d7771fde38d64c5d82dd81c6290b
samuel <samuelgomes101112@gmail.com>
2024-12-29 20:09:06 -0300

    it Shold be able to create a new question bigger than 255 characters

---
+++ anotacoes.js
@@ -0,0 +1,33 @@
+
+
+// Para aux. no git hooks, para que fique salvo no git os commits padroes do projeto, iremos instalar um pacote JS:
+// --husky git hooks https://typicode.github.io/husky/
+
+
+// LARAVEL PINT
+// formatador de codigo, mais bonito
+
+
+//LARASTAN
+// https://github.com/larastan/larastan
+// foca em encontrar erros no seu código .Ele captura classes inteiras de bugs antes mesmo de você escrever testes para o código
+
+
+//Pre commit
+// git hook pre-commit, toda vez que fizer o commit será rodado o LARASTAN e o PINT formatando e adicionando no commit
+
+
+//Git Actions
+// A cada pull-request que for aberto tera que rodar os testes
+
+//DebugBar
+// https://github.com/barryvdh/laravel-debugbar
+//Mostrar no navegador varias informaçoes sobre o projeto
+
+//TDD
+// Sao divididos entre testes unitarios UNIT e testes de FEATURE que contempla tudo
+// Iniciando com os testes em desenvolvimento é interessante apagar a pasta Unit e excluir um codigo do arquivo phpunit.xml
+// Os testes serao pelo PEST. php ./vendor/bin/pest --help
+// adicionar o banco de dados sqlite dentro do phpunit.xml para os testes serem mais rapidos
+//Criar um arquivo dentro de Feature para testes, CreateAQuestionTest.php
+// php artisan test --filter=Create
